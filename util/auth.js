18ee95abcd99a3b266a8fef90a33c7acfa640757
Hessam <hessam.imen@gmail.com>
2023-09-20 17:00:33 +1000

    feat: implement a function to send athuntication requests to the backend

---
+++ util/auth.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyBYrk2qKttCZLfiJnEgRn5R-g8Pb52AwzI";
+
+async function createUser(email, password) {
+  const response = await axios.post(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${API_KEY}`,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    }
+  );
+}
+
+export { createUser };
