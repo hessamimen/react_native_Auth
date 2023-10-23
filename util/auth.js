44b64db26952866bad16abd82db70e0e679c0870
Hessam <hessam.imen@gmail.com>
2023-10-23 21:07:25 +1100

    feat: user login implementation

--- util/auth.js
+++ util/auth.js
@@ -2,15 +2,20 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBYrk2qKttCZLfiJnEgRn5R-g8Pb52AwzI";
 
-async function createUser(email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+async function authenticate(mode, email, password) {
+  const URL = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const response = await axios.post(URL, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
 }
 
-export { createUser };
+export async function createUser(email, password) {
+  authenticate("signUp", email, password);
+}
+export async function login(email, password) {
+  authenticate("signInWithPassword", email, password);
+}
