fbfe37d28b3124580072409a8681c855c8c4ef72
Hessam <hessam.imen@gmail.com>
2023-10-23 21:33:14 +1100

    feat: authentication error handeling

--- util/auth.js
+++ util/auth.js
@@ -3,8 +3,9 @@ import axios from "axios";
 const API_KEY = "AIzaSyBYrk2qKttCZLfiJnEgRn5R-g8Pb52AwzI";
 
 async function authenticate(mode, email, password) {
-  const URL = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
-  const response = await axios.post(URL, {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
     email: email,
     password: password,
     returnSecureToken: true,
@@ -14,8 +15,9 @@ async function authenticate(mode, email, password) {
 }
 
 export async function createUser(email, password) {
-  authenticate("signUp", email, password);
+  await authenticate("signUp", email, password);
 }
+
 export async function login(email, password) {
-  authenticate("signInWithPassword", email, password);
+  await authenticate("signInWithPassword", email, password);
 }
