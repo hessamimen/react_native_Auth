14bbaa7ca71636d80d68e77ab1583110f06c5b7a
Hessam <hessam.imen@gmail.com>
2023-10-23 21:05:23 +1100

    fix: update the create user URL

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import AuthContent from '../components/Auth/AuthContent';
+import AuthContent from "../components/Auth/AuthContent";
 
 function LoginScreen() {
   return <AuthContent isLogin />;
--- util/auth.js
+++ util/auth.js
@@ -4,7 +4,7 @@ const API_KEY = "AIzaSyBYrk2qKttCZLfiJnEgRn5R-g8Pb52AwzI";
 
 async function createUser(email, password) {
   const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${API_KEY}`,
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
     {
       email: email,
       password: password,
