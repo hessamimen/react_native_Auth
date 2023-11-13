f6e916f4fa52fc72a0cf2f6dd767aa70eb3a7bcc
Hessam <hessam.imen@gmail.com>
2023-11-13 15:51:31 +1100

    feat: final commit

--- util/auth.js
+++ util/auth.js
@@ -20,6 +20,5 @@ export function createUser(email, password) {
 }
 
 export function login(email, password) {
-  console.log(email, password);
   return authenticate("signInWithPassword", email, password);
 }
